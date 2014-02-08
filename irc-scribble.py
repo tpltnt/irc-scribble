f8a4d9943109f31449bde64b69fc9a5e4bd9c075
tpltnt <tpltnt.github@dropcut.net>
2014-02-08 18:30:49 +0100

    basic socket handling added

--- irc-scribble.py
+++ irc-scribble.py
@@ -1,3 +1,23 @@
 #!/usr/bin/env python3
 
-print("hi there")
+"""
+A very basic script to leave messages for others in an IRC channel.
+
+:author: tpltnt
+:license: AGPLv3 (or later)
+"""
+
+import socket
+import sys
+
+
+# open a socket to talk to a freenode IRC server (in clear text)
+try:
+    connection = socket.create_connection(("chat.freenode.net", 6666))
+except OSError:
+    # close on any error
+    print("can't open the connection")
+    sys.exit(1)
+
+# close the connection
+connection.close()
