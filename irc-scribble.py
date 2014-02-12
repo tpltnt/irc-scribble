3cfba448d35c27006881e8988cc61c608b7d0d27
tpltnt <tpltnt.github@dropcut.net>
2014-02-12 19:47:18 +0100

    PING-PONG added

--- irc-scribble.py
+++ irc-scribble.py
@@ -10,7 +10,7 @@ A very basic script to leave messages for others in an IRC channel.
 import socket
 import sys
 
-botname = "irc_scribble_bot"
+botname = "irc_testbot"
 channel = "#ircscribble"
 
 
@@ -36,19 +36,36 @@ while((b'MODE ' + bytes(botname, 'ascii') + b' :+i') not in serverbytes):
     if b':Nickname is already in use.' in serverbytes:
         print("nick already in use")
         sys.exit(2)
-print(serverbytes)
+#print(serverbytes)
+
 
 # join a channel
 cbytes = b'JOIN ' + bytes(channel, 'ascii') + b'\r\n'
 if len(cbytes) != connection.send(cbytes):
     print("an error occured")
-
+serverbytes = connection.recv(4096)
+clientstring = ''
+if b'JOIN' in serverbytes:
+    clientstring = str(serverbytes).split(' ')[0]
+    clientstring = clientstring.split('!')[1]
 #
 # do you magic bot action here
 #
-cbytes = b'PRIVMSG ' + bytes(channel, 'ascii') +  b' :howdy!\r\n'
+cbytes = b'PRIVMSG ' + bytes(channel, 'ascii') +  b' :i am a bot, nothing special\r\n'
 if len(cbytes) != connection.send(cbytes):
     print("an error occured")
+serverbytes = connection.recv(4096)
+## receive data/read into bytearray until bot is accepted
+while((b'go away') not in serverbytes):
+    # handle PING-PONG
+    if b'PING' in serverbytes:
+        cbytes = b'PONG :' + bytes(clientstring, 'ascii') +  b'\r\n'
+        if len(cbytes) != connection.send(cbytes):
+            print("an error occured during PING-PONG")
+    print(serverbytes)
+    serverbytes = connection.recv(4096)
+
+print(serverbytes)
 
 # leave the channel
 cbytes = b'QUIT :Gone to data heaven\r\n'
